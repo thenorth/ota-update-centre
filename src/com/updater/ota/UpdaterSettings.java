5ca1be783206c76207f7ecbe5fb87dbaf99a4cc4
JieeHD <jordan.craig94@gmail.com>
2012-08-21 12:31:28 +0100

    Small addition to last commit, far from finished. Adds writing to shared prefs for reading from later on.

--- src/com/updater/ota/UpdaterSettings.java
+++ src/com/updater/ota/UpdaterSettings.java
@@ -1,5 +1,6 @@
 package com.updater.ota;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
@@ -10,6 +11,9 @@ import android.preference.PreferenceActivity;
 public class UpdaterSettings extends PreferenceActivity {
 	
 	private CheckBoxPreference show_notif;
+	private SharedPreferences prefs;
+	private String SHOW_NOTIF_KEY = "show_notifications";
+	private SharedPreferences.Editor editor;
 	
 
 	@Override
@@ -18,15 +22,32 @@ public class UpdaterSettings extends PreferenceActivity {
 		addPreferencesFromResource(R.xml.settings_main);
 		
 		show_notif = (CheckBoxPreference) findPreference("show_notif");
+		
+		prefs = getPreferences(MODE_PRIVATE);
+		editor = prefs.edit();
+		
+		prefs.getBoolean(SHOW_NOTIF_KEY, true);
+		if (SHOW_NOTIF_KEY == null) {
+			editor.putBoolean(SHOW_NOTIF_KEY, true);
+		} 
+		
+		
 		show_notif.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 
 			@Override
 			public boolean onPreferenceClick(Preference preference) {
 				// TODO Auto-generated method stub
+				if (show_notif.isChecked()) {
+					editor.putBoolean(SHOW_NOTIF_KEY, true);
+				} else {
+					editor.putBoolean(SHOW_NOTIF_KEY, false);
+				}
 				return false;
 			}
 			
 		});
 	}
+	
+
 
 }
